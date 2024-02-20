6ddb160d23c4eb9175fb3e7bf8f8a4cd4732fb65
antoine-codeur <antoinejf.denoue@gmail.com>
2024-02-20 09:25:27 +0100

    Commentaires retirés

--- asset/script/country.js
+++ asset/script/country.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Chemin vers votre fichier JSON
     const jsonPath = 'asset/script/json/countries_and_nationalities.json';
 
     fetch(jsonPath)
@@ -9,20 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
             const nationalitiesElement = document.getElementById('nationalities');
 
             data.forEach(item => {
-                // Ajout des pays au datalist des pays
                 let optionCountry = document.createElement('option');
                 optionCountry.value = item.country;
                 countriesElement.appendChild(optionCountry);
 
-                // Ajout des nationalités au datalist des nationalités
                 let optionNationality = document.createElement('option');
                 optionNationality.value = item.nationality;
                 nationalitiesElement.appendChild(optionNationality);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
-
-    // Gestionnaire pour la saisie sur le champ de nationalité
     document.getElementById('nationality').addEventListener('input', function() {
         const selectedNationality = this.value;
         fetch(jsonPath)
